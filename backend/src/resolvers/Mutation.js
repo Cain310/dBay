645047b76ceb37807b05bd21a36744b160723760
Michael Cain <michaelcaincb@gmail.com>
2019-07-04 12:31:42 -0400

    implemented backend user auth via cookie and jwt graphql cookie parser

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -40,6 +43,33 @@ const Mutations = {
     // TODO
     // Delete it
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    //  lowercase their email
+    args.email = args.email.toLowerCase();
+    //  hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    //  create the user in the database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ["USER"] }
+        }
+      },
+      info
+    );
+    //  create the JWT for user
+    const token = jwt.sign({ userID: user.id }, process.env.APP_SECRET);
+    //  We set the jwt as a cookie on the response
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      // 1 year cookie
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+    // we return the user to the browser
+    return user;
   }
 };
 
