e23297712e518ac42c8407cc06d3fe91fabfed28
Michael Cain <michaelcaincb@gmail.com>
2019-08-02 16:07:58 -0400

    adding user permission ui interface and queries

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,5 +1,5 @@
 const { forwardTo } = require("prisma-binding");
-
+const { hasPermission } = require("../utils");
 const Query = {
   items: forwardTo("db"),
   item: forwardTo("db"),
@@ -16,6 +16,17 @@ const Query = {
       },
       info
     );
+  },
+  async users(parent, args, ctx, info) {
+    // 1. check if the user is logged in
+    if (!ctx.request.userId) {
+      throw new Error("You must be logged in!");
+    }
+    // 2. check if the user has the permissions to query all the users
+    hasPermission(ctx.request.user, ["ADMIN", "PERMISSIONUPDATE"]);
+
+    // 3. if they do query all the users!
+    return ctx.db.users({}, info);
   }
 };
 
